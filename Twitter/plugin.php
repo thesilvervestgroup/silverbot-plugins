cd8477524ee24a2ad0887be8bde8a26da48faaac
Nick Silvestro <nick@silvervest.net>
2011-06-09 11:24:36 +1000

    added general twitter plugin

---
+++ Twitter/plugin.php
@@ -0,0 +1,41 @@
+<?php
+
+class Twitter extends SilverBotPlugin {
+
+	public function chn_twitter($data) {
+		if (empty($data['data']))
+			return;
+			
+		list($username) = explode(' ', $data['data'], 1);
+		$tweet = $this->getLastTweet($username);
+		$tweet = str_replace("\n", " ", $tweet); // because charliesheen is annoying
+		
+		if (empty($tweet))
+			$this->bot->reply("No recent tweets found for '$username'");
+		else
+			$this->bot->reply("@$username: $tweet");
+	}
+
+	private function getLastTweet($username) {
+		$url = 'http://api.twitter.com/1/statuses/user_timeline.json?screen_name='.$username.'&trim_user=true&count=1&include_rts=true';
+
+		$data = json_decode($this->curlGet($url));
+		if (!is_array($data)) return false;
+		
+		$tweet = $data[0];
+		return $tweet->text;
+	}
+
+	private function curlGet($url) {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HEADER, false);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$output = curl_exec($ch);
+		curl_close($ch);
+		return $output;
+	}
+	
+}
+
+
