ba92ecc53060a90e4036688aff54b06c7f090795
Nick Silvestro <nick@silvervest.net>
2011-07-06 17:16:36 +1000

    fukung plugin!

---
+++ Fukung/plugin.php
@@ -0,0 +1,43 @@
+<?php
+
+class Fukung extends SilverBotPlugin {
+
+	public function chn_fukung($data) {
+		if (empty($data['data'])) {
+			$this->random();
+		} else {
+			$this->search($data['data']);
+		}
+	}
+
+	private function random() {
+		$url = 'http://fukung.net/random';
+		$data = $this->curlGet($url);
+		
+		if (preg_match('/link\s+rel=\"image_src\"\s+href=\"(.+)\"/', $data, $matches) !== false) {
+			$this->bot->reply(strip_tags(trim($matches[1])));
+		}
+	}
+		
+	private function search($term) {
+		$url = 'http://fukung.net/rss/tag/200/' . urlencode($term);
+		$data = $this->curlGet($url);
+		
+		$lines = explode("\n", $data);
+		$images = array();
+		foreach ($lines as $line) {
+			if (preg_match('/href=\"http:\/\/fukung.net\/v\/(.+?)\"><img/i', $line, $matches) != false) {
+				$images[] = "http://media.fukung.net/images/" . $matches[1];
+			}
+		}
+		
+		$count = count($images);
+		if ($count) {
+			$rand = mt_rand(0, $count);
+			$image = $images[$rand];
+			$this->bot->reply("$term: $image ($rand of $count)");
+		}
+	}
+
+}
+
