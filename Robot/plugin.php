a9a1f7edb29b3dc4d4a49dd5a59d2d52a5c1bb35
jadz <kippage@gmail.com>
2012-05-20 22:03:05 +1000

    Added pinppage support

--- Robot/plugin.php
+++ Robot/plugin.php
@@ -31,6 +31,7 @@ class Robot extends SilverBotPlugin {
 			} else {
 				$this->data['urls'][$channel][$url]['w'] = time();
 				$this->data['urls'][$channel][$url]['u'] = $data['username'];
+                $this->_postPin($url, $data['username']);
 			}
 		}
 
@@ -243,5 +244,28 @@ class Robot extends SilverBotPlugin {
 		$text = "$difference $periods[$j] ago";
 		return $text;
 	}
+
+    private function _postPin($sourceUrl, $from)
+    {
+        if(empty($sourceUrl) || empty($from)) return;
+
+        $url     = 'http://serv.pinppage.com/v1.0/pin';
+        $request = 'url=' . urlencode($sourceUrl) . '&from=' . urlencode($from);
+        $length  = strlen($request);
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $request);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $result = curl_exec($ch);
+        $ok     = curl_getinfo($ch, CURLINFO_HTTP_CODE) == 200;
+
+        curl_close($ch);
+
+        return $ok;
+    }
 }
 
