aff77e5534be586b216da2dd273c3b371058cd07
Nick Silvestro <nick@silvervest.net>
2012-08-03 12:26:19 +1000

    added youtube title echo

--- YouTube/plugin.php
+++ YouTube/plugin.php
@@ -18,6 +18,16 @@ class YouTube extends SilverBotPlugin {
 		$this->chn_youtube($data);
 	}
 	
+	public function chn($data) {
+		$regexp = '~https?://(?:[0-9A-Z-]+\.)?(?:youtu\.be/|youtube\.com\S*[^\w\-\s])([\w\-]{11})(?=[^\w\-]|$)(?![?=&+%\w]*(?:[\'"][^<>]*>| </a>))[?=&+%\w-]*~ix';
+		if (preg_match($regexp, $data['text'], $matches) != false) {
+			$url = "http://gdata.youtube.com/feeds/api/videos/". $matches[1] . "?alt=json";
+			$data = json_decode($this->curlGet($url), true);
+
+			$this->bot->reply("YouTube - {$data['entry']['title']['$t']} ({$matches[1]})");
+		}
+	}
+	
 	private function searchVideo($term) {
 		$url = 'http://gdata.youtube.com/feeds/api/videos?alt=json&q=' . urlencode($term);
 		$data = json_decode($this->curlGet($url));
